366f86ad0afb67e2c98c39e61fc2cfe9d5515185
PEDRO HENRIQUE RECH SARMENTO <pedro.sarmento@acad692.com.br>
2019-05-31 19:59:46 -0300

    Solução: Sistema de Locação de Veiculos

---
+++ SLV/SLV/Models/Veiculo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SLV.Models
+{
+    public class Veiculo
+    {
+        public int IdVeiculo { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Placa { get; set; }
+        public double ValorFipe { get; set; }
+        public DateTime AnoFabricacao { get; set; }
+        public DateTime UltimaRevisao { get; set; }
+    }
+}
