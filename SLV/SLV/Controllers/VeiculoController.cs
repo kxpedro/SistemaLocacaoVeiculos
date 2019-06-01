028efefa4d766d9c67b4c1259771d06f01b8898d
PEDRO HENRIQUE RECH SARMENTO <pedro.sarmento@acad692.com.br>
2019-05-31 21:35:59 -0300

    SLV

---
+++ SLV/SLV/Controllers/VeiculoController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SLV.Database;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SLV.Controllers
+{
+    public class VeiculoController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var veiculos = new Models.Veiculo();
+
+            return View(veiculos);
+        }
+
+        [HttpGet]
+        public ActionResult Details(int idVeiculo)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Post(Models.Veiculo v)
+        {
+            var sql = Connection.Conectar();
+
+            SqlCommand cmd = new SqlCommand("SELECT * FROM TableFuncionarios", sql);
+            SqlDataAdapter da = new SqlDataAdapter();
+            DataSet ds = new DataSet();
+
+
+            return View();
+        }
+
+        [HttpPut]
+        public ActionResult Put(int idVeiculo)
+        {
+            return View();
+        }
+
+        [HttpDelete]
+        public ActionResult Delete(int idVeiculo)
+        {
+            return View();
+        }
+    }
+}
--- SLV/SLV/Models/Veiculo.cs
+++ SLV/SLV/Models/Veiculo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +8,19 @@ namespace SLV.Models
 {
     public class Veiculo
     {
+        [DisplayName("Identificação do Veiculo")]
         public int IdVeiculo { get; set; }
+        [DisplayName("Marca do Veiculo")]
         public string Marca { get; set; }
+        [DisplayName("Modelo do Veiculo")]
         public string Modelo { get; set; }
+        [DisplayName("Placa do Veiculo")]
         public string Placa { get; set; }
+        [DisplayName("Valor na Tabela Fipe do Veiculo")]
         public double ValorFipe { get; set; }
+        [DisplayName("Ano de Fabricacao do Veiculo")]
         public DateTime AnoFabricacao { get; set; }
+        [DisplayName("Ultima Revisao do Veiculo")]
         public DateTime UltimaRevisao { get; set; }
     }
 }
