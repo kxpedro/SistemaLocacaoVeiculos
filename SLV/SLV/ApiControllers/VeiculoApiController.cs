b4885811a64fff35d7f34b0f897aa6c0f941dd6f
kxpedro <pedrorshenrique1515@hotmail.com>
2019-06-07 08:24:30 -0300

    Api Controllers

---
+++ SLV/SLV/ApiControllers/VeiculoApiController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SLV.ApiControllers
+{
+    public class VeiculoApiController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[];
+        }
+
+        // GET api/<controller>/5
+        public string GetById(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
